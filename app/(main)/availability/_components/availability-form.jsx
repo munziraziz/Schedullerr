d789da7c607a9a45777663729b25657d7f69b212
munziraziz <munzirwrites@gmail.com>
2026-02-18 15:31:13 +0500

    6th commit

--- app/(main)/availability/_components/availability-form.jsx
+++ app/(main)/availability/_components/availability-form.jsx
@@ -1,15 +1,121 @@
-"use client"
+"use client";
 
-import React from 'react'
+import { availabilitySchema } from "@/app/_lib/validators";
+import { Checkbox } from "@/components/ui/checkbox";
+import {
+  Select,
+  SelectTrigger,
+  SelectValue,
+  SelectGroup,
+  SelectContent,
+  SelectItem,
+} from "@/components/ui/select";
+import { zodResolver } from "@hookform/resolvers/zod";
+import React from "react";
+import { Controller, useForm } from "react-hook-form";
+import { timeSlots } from "../data";
 
-const AvailabilityForm = ({initialData}) => {
-    console.log(initialData)
+const AvailabilityForm = ({ initialData }) => {
+  const { register, handleSubmit, control, setValue, watch } = useForm({
+    resolver: zodResolver(availabilitySchema),
+    defaultValues: { ...initialData },
+  });
   return (
-    <div className="availability-form">
-      <h2>Availability Form</h2>
+    <form>
+      {[
+        "monday",
+        "tuesday",
+        "wednesday",
+        "thursday",
+        "friday",
+        "saturday",
+        "sunday",
+      ].map((day) => {
+        const isAvailable = watch(`${day}.isAvailable`);
 
-    </div>
-  )
-}
+        return (
+          <div key={day} className="flex items-center space-x-4 mb-4">
+            <Controller
+              name={`${day}.isAvailable`}
+              control={control}
+              render={({ field }) => {
+                return (
+                  <Checkbox
+                    checked={field.value}
+                    onCheckedChange={(checked) => {
+                      setValue(`${day}.isAvailable`, checked);
+                      if (!checked) {
+                        setValue(`${day}.startTime`, "9:00");
+                        setValue(`${day}.endTime`, "17:00");
+                      }
+                    }}
+                  />
+                );
+              }}
+            />
+            <span className="w-24">
+              {day.charAt(0).toUpperCase() + day.slice(1)}
+            </span>
+            {isAvailable && (
+              <>
+                <Controller
+                  name={`${day}.startTime`}
+                  control={control}
+                  render={({ field }) => {
+                    return (
+                      <Select
+                          onValueChange={field.onChange}
+                      value = {field.value}
+                      >
+                        <SelectTrigger className="w-32">
+                          <SelectValue placeholder="Start Time" />
+                        </SelectTrigger>
+                        <SelectContent>
+                          {timeSlots.map(time => {
+                            return(
+                            <SelectItem key={time} value={time}>
+                                {time}
+                                </SelectItem>
+                            );
+                          })}
+                        </SelectContent>
+                      </Select>
+                    );
+                  }}
+                />
+                <span>to</span>
+                <Controller
+                  name={`${day}.endTime`}
+                  control={control}
+                  render={({ field }) => {
+                    return (
+                      <Select
+                      onValueChange={field.onChange}
+                      value = {field.value}
+                      >
+                        <SelectTrigger className="w-32">
+                          <SelectValue placeholder="End Time" />
+                        </SelectTrigger>
+                        <SelectContent>
+                          {timeSlots.map(time => {
+                            return(
+                            <SelectItem key={time} value={time}>
+                                {time}
+                                </SelectItem>
+                            );
+                          })}
+                        </SelectContent>
+                      </Select>
+                    );
+                  }}
+                />
+              </>
+            )}
+          </div>
+        );
+      })}
+    </form>
+  );
+};
 
-export default AvailabilityForm
+export default AvailabilityForm;
